3d816c1eae6169cf018f9dd2f432b39804b0133a
Irvin Lu <scurvyirv@scurvyirvy.attlocal.net>
2024-08-23 13:56:00 -0700

    feat: updated content logic

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+//component for footer
+function Footer() {
+  return <footer>this is the bottom third</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+// this component includes the nav bar and its JS
+function Header(props) {
+  return (
+    <header>
+      <h1> Portfile </h1>
+      <nav>
+        <button onClick={() => props.setCurrentPage("AboutMe")}>
+          about me
+        </button>
+        <button onClick={() => props.setCurrentPage("Contact")}>contact</button>
+        <button onClick={() => props.setCurrentPage("Projects")}>
+          projects
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
